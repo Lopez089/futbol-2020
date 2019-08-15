34cd558521afe0d33fc7c3ec0ba4c00c619fdf62
Juan Lopez <contacto@juanlopezaragon.es>
2019-08-15 09:48:14 +0200

    todas la funciones

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-let express =  require("express");
-let app = express();
+//////////////////////////////////////////////////////////////////
+//////////////////            REQUERIDOS       //////////////////
+//////////////////////////////////////////////////////////////////
 
-// IMPORT
-
+const express =  require("express");
+const app = express();
 const request = require('request');
 const cheerio = require('cheerio');
 const _ = require('lodash');
-var equiposTT = []
-var equiposTs
-let equiposPorJugar
-// DATOS
+
+//////////////////////////////////////////////////////////////////
+//////////////////            DATOS             //////////////////
+//////////////////////////////////////////////////////////////////
 
 const ligas = [
     "https://es.soccerway.com/national/england/premier-league/20192020/regular-season/r53145/matches/?ICID=PL_3N_02",//0
@@ -20,56 +21,65 @@ const ligas = [
     "https://es.soccerway.com/national/netherlands/eredivisie/20192020/regular-season/r54058/matches/?ICID=PL_3N_02",
     "https://es.soccerway.com/national/turkey/super-lig/20192020/regular-season/r53866/matches/?ICID=PL_3N_02",
     "https://es.soccerway.com/national/portugal/portuguese-liga-/20192020/regular-season/r53517/matches/?ICID=PL_3N_02",
-    "https://es.soccerway.com/national/belgium/pro-league/20192020/regular-season/r53516/?ICID=HP_POP_11",    
-    "https://es.soccerway.com/national/turkey/super-lig/20192020/regular-season/r53866/?ICID=HP_POP_07"
+    "https://es.soccerway.com/national/belgium/pro-league/20192020/regular-season/r53516/?ICID=HP_POP_11",
+    "https://es.soccerway.com/national/turkey/super-lig/20192020/regular-season/r53866/?ICID=HP_POP_07",
+    "https://el.soccerway.com/national/spain/segunda-division/20192020/regular-season/r54950/matches/?ICID=PL_3N_02"
 ]
 
 const direcionBase = "https://es.soccerway.com"
 
 // variables locales
-let GanadorFilter
-// comparar las fechas
-const fecha = (fecha)=>{
-//console.log(fecha)
-// fecha de hoy
-const hoy = new Date()
-let dia = hoy.getDate();
-let mes = hoy.getMonth()+1;
-const ano = hoy.getFullYear();
-dia = dia<10? dia = '0'+dia : dia
-mes = mes<10? mes = '0'+mes : mes
-const fechaActual = String(ano+"/"+mes+"/"+dia);
+let GanadorFilter, quiniela,masDosFilter, GanadorMasDosFilter, AmbosFilter, AmbosMasDosFilter , masCeroCinco, equiposPorJugar
+let datoss = []
+//////////////////////////////////////////////////////////////////
+//////////////////        COMPARAR FECHA        //////////////////
+//////////////////////////////////////////////////////////////////
 
-//console.log(fechaActual)
-return fecha === fechaActual ?  true : false;
+const fecha = (fecha)=>{
+
+    // fecha de hoy
+    const hoy = new Date() // '2019/08/16'
+    let dia = hoy.getDate();
+    let mes = hoy.getMonth()+1;
+    const ano = hoy.getFullYear();
+    dia = dia<10? dia = '0'+dia : dia
+    mes = mes<10? mes = '0'+mes : mes
+        const fechaActual = String(ano+"/"+mes+"/"+dia);
+
+    //console.log(fechaActual)
+    return fecha === fechaActual ?  true : false;
 }
 
-// horas
+//////////////////////////////////////////////////////////////////
+//////////////////        COMPARAR HORAS        //////////////////
+//////////////////////////////////////////////////////////////////
 
 const hora=(hora)=>{
 
-    let d = new Date();
+    const d = new Date(); // '00 : 00'
     let horas = d.getUTCHours()+2
     let minuto = d.getMinutes()
 
-    horas<10? horas = '0'+horas : horas
-    minuto<10? minuto = '0'+minuto : minuto
-    
-    const horaActual = horas + ' : '+ minuto
-    //console.log(horaActual)
-    //console.log(hora)
+        horas<10? horas = '0'+horas : horas
+        minuto<10? minuto = '0'+minuto : minuto
+
+        const horaActual = horas + ' : '+ minuto
+        //console.log(horaActual)
+        //console.log(hora)
     return hora >= horaActual ? true : false
     }
 
-// 22:00    11:00
+//////////////////////////////////////////////////////////////////
+//////////////////        APP                   //////////////////
+//////////////////////////////////////////////////////////////////
 
-let datoss = []
+
 const datos = (equiposHoy)=>{
-    
+
     equiposHoy.map(async(p,i)=>{
-    
+
         await request(p.url, p.hora, function (error, response, body){
-               // console.log(p.url)
+                //console.log(p.url)
             let $ = cheerio.load(body);
             equiposT = $(`#bd`)
                 let partido = equiposT.find("#subheading h1").text()
@@ -81,14 +91,12 @@ const datos = (equiposHoy)=>{
                 let E2EncajadoVisita = equiposT.find("td.team_b_col.away").eq("10").text()
                 let E2MarcadosVisita = equiposT.find("td.team_b_col.away").eq("9").text()
                 let E1EncajadoCasa =equiposT.find("td.team_a_col.home").eq("10").text().slice(0, -7)
-            
-                var GeneralResultado1 =  Math.min(E1MarcadoGenera, E2EncajadGenera);
+                let GeneralResultado1 =  Math.min(E1MarcadoGenera, E2EncajadGenera);
                 let GeneralResultado2 =  Math.min(E2MarcadoGenera, E1EncajadGenera);
                 let GeneralLocal =  Math.min(E1MarcadoCasa, E2EncajadoVisita);
                 let Generalvisitante =  Math.min(E2MarcadosVisita, E1EncajadoCasa);
-    
                 //console.log(partido + Generalvisitante)
-            
+
                     a={
                         eq1:equiposT.find(`.logo-wrapper.left a`).text(),
                         eq2:equiposT.find(`.logo-wrapper.right a`).text(),
@@ -101,55 +109,72 @@ const datos = (equiposHoy)=>{
                         ResultadoFinal2: Math.floor(Math.min(GeneralResultado2, Generalvisitante)),
                         hora: p.hora,
                         horaPasa: hora(p.hora),
-                        jornada: Math.floor(equiposT.find('.block_h2h_general_statistics .compare .first.even .team_a_col.total').eq('1').text() )+ 1
-                        
+                        jornada: Math.floor(equiposT.find('.block_h2h_general_statistics .compare .first.even .team_a_col.total').eq('0').text() )+ 1,
+                        url: p.url
                     }
-            datoss.push(a)
-            datossOrd =  _.orderBy(datoss, ['hora'], ['asc']);
-            //console.log(datossOrd)
-            equiposPorJugar = datossOrd.filter(hora => hora.horaPasa === true)
-            console.log(equiposPorJugar)
+                        datoss.push(a)
+                        datossOrd =  _.orderBy(datoss, ['hora'], ['asc']);
+                        //console.log(datossOrd)
+                        equiposPorJugar = datossOrd.filter(hora => hora.horaPasa === true && hora.jornada >= 4)
+                        //console.log(equiposPorJugar)
 
-                // FILTRAR POR GANADOR
-                GanadorFilter =  _.filter(equiposPorJugar , (o)=> {
-                    let r = o.ResultadoFinal1 - o.ResultadoFinal2
-                    //console.log(r)
-                    //console.log('resultado ')
-                    return r >=2 || r <= -2})
+                            // FILTRAR POR GANADOR
+                                GanadorFilter =  _.filter(equiposPorJugar , (o)=> {
+                                    let r = o.ResultadoFinal1 - o.ResultadoFinal2
+                                    //console.log(r)
+                                    //console.log('resultado ')
+                                    return r >=2 || r <= -2})
+                                    //console.log(GanadorFilter)
+                            // FILTRAR AMBOS EQUIPOS MARCAN
+                                AmbosFilter =  _.filter(equiposPorJugar , (o)=> {
+                                    return o.ResultadoFinal1 > 0 && o.ResultadoFinal2 > 0
+                                })
 
-                // FILTRAR AMBOS EQUIPOS MARCAN
-                AmbosFilter =  _.filter(equiposPorJugar , (o)=> {
-                    return o.ResultadoFinal1 > 0 && o.ResultadoFinal2 > 0
-                })
+                            // FILTRAR +2,5
+                            masDosFilter =  _.filter(equiposPorJugar , (o)=> {
+                                let s = o.ResultadoFinal1 + o.ResultadoFinal2
+                                return s >= 3
+                            })
 
-                // FILTRAR +2,5
-                masDosFilter =  _.filter(equiposPorJugar , (o)=> {
-                    let s = o.ResultadoFinal1 + o.ResultadoFinal2 
-                    return s >= 3
-                })
+                            // FILTRAR GANADOR +2,5
+                            GanadorMasDosFilter =  _.filter(equiposPorJugar , (o)=> {
+                                let r = o.ResultadoFinal1 - o.ResultadoFinal2
+                                let s = o.ResultadoFinal1 + o.ResultadoFinal2
+                                //console.log(r)
+                                //console.log('resultado ')
+                                return r >=2 || r <= -2 && s >= 3
+                            })
 
-                // FILTRAR GANADOR +2,5
-                GanadorMasDosFilter =  _.filter(equiposPorJugar , (o)=> {
-                    let r = o.ResultadoFinal1 - o.ResultadoFinal2
-                    let s = o.ResultadoFinal1 + o.ResultadoFinal2
-                    //console.log(r)
-                    //console.log('resultado ')
-                    return r >=2 || r <= -2 && s >= 3
-                })
-
-                // FILTRAR AMBOS MARCAN +2,5
-                AmbosMasDosFilter =  _.filter(equiposPorJugar , (o)=> {
-                    let r = o.ResultadoFinal1 > 0 && o.ResultadoFinal2 > 0
-                    let s = o.ResultadoFinal1 + o.ResultadoFinal2
-                    //console.log(r)
-                    //console.log('resultado ')
-                    return o.ResultadoFinal1 > 0 && o.ResultadoFinal2 > 0 && s >= 3
-                })
+                            // FILTRAR AMBOS MARCAN +2,5
+                            AmbosMasDosFilter =  _.filter(equiposPorJugar , (o)=> {
+                                let r = o.ResultadoFinal1 > 0 && o.ResultadoFinal2 > 0
+                                let s = o.ResultadoFinal1 + o.ResultadoFinal2
+                                //console.log(r)
+                                //console.log('resultado ')
+                                return o.ResultadoFinal1 > 0 && o.ResultadoFinal2 > 0 && s >= 3
+                            })
+                            // FILTRAR Quiniela
+                            quiniela =  _.filter(datossOrd , (o)=> {
+                                let e = o.liga
+                                //console.log(r)
+                                //console.log('resultado ')
+                                return e === 'España'
+                            })
+                                //console.log(datossOrd)
+                            // FILTRAR  +0,5
+                            masCeroCinco =  _.filter(equiposPorJugar , (o)=> {
+                                let r = o.ResultadoFinal1 >= 1 || o.ResultadoFinal2 >= 1
+                                //console.log(r)
+                                //console.log('resultado ')
+                                return r
+                            })
     })
 })
 }
 
-// EXTRAER URL PARTIDO
+//////////////////////////////////////////////////////////////////
+//////////////////       TRAER URL              //////////////////
+//////////////////////////////////////////////////////////////////
 const url = ()=>{
     ligas.map( async (l, i) => {
         await request (l, (error, response, body) => {
@@ -173,43 +198,9 @@ const url = ()=>{
 }
 url()
 
-
-// TODO:
-// crear la hora actual 
-// compara si es la hora actual
-
-
-
-
-
-
-
-//MOSTRAR TODAS LA URL PARTIDOS
-
-const mostrarUrlPartido = ()=>{
-    equipos.map(async url=> console.log(url.url))
-    console.log('ya esta todas la url extraidas')
-}
-
-// EXTRAER URL COMPARACION
-
-const urlsCompara = ()=>{
-
-    equipos.map( (l,i)=>{
-        request(l.url, function (error, response, body) {
-            let $ = cheerio.load(body);
-            urlCompara = $(`#submenu`).toArray().map( async equipoComp => {
-                const $equipoComp = $(equipoComp)
-                return {
-                    url: direcionBase + $equipoComp.find("a").attr("href") + '/head2head/',
-                }
-            })
-            //console.log( urlCompara)
-        })
-    })
-}
-
-//
+//////////////////////////////////////////////////////////////////
+//////////////////            SERVIDOR          //////////////////
+//////////////////////////////////////////////////////////////////
 
 
 app.set('views', './dev/layaut')
@@ -217,9 +208,7 @@ app.set("view engine", "pug")
 app.use(express.static('./src'));
 app.get("/", function(req,res){
     let titulo = 'Ganador'
-    res.render('indexPrueba', {equipos: GanadorFilter, titulo });
-    console.log('servidor cargado')
-
+    res.render('indexPrueba', {equipos: GanadorFilter, titulo })
 });
 
 app.get("/2,5", function(req,res){
@@ -247,7 +236,17 @@ app.get("/Pruevas", function(req,res){
     res.render('indexPrueba', {equipos: AmbosFilter, titulo })
 })
 
-app.set("port", process.env.PORT || 3030);
+app.get("/masCeroCinco", function(req,res){
+    let titulo = 'Mas 0,5'
+    res.render('indexPrueba', {equipos:masCeroCinco, titulo })
+})
+
+app.get("/quiniela", function(req,res){
+    let titulo = 'Quiniela'
+    res.render('indexPrueba', {equipos: quiniela, titulo })
+})
+
+app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), () =>{
 
 });
